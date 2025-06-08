c4091c2ea8b90597b7b1a77d2bb87f86b7e6674b
MaciejUlasiuk <filip123xd@o2.pl>
2025-06-08 21:35:39 +0200

    cors add

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path'); 
 const app = express();
+const cors = require('cors');
+
 
 const currencyRoutes = require('./routes/currencyRoutes');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
