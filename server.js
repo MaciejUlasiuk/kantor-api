65bebb34ddcf335ee1bbbf72b3f42256efa26ecf
MaciejUlasiuk <filip123xd@o2.pl>
2025-06-08 21:19:19 +0200

    simple html add

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const path = require('path'); 
 const app = express();
 
-const currencyRoutes = require('./routes/currencyRoutes'); 
+const currencyRoutes = require('./routes/currencyRoutes');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Witaj w Kantor API!');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api-welcome', (req, res) => { 
+  res.send('Kantor api');
 });
 
-app.use('/api', currencyRoutes); 
+app.use('/api', currencyRoutes);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Serwer uruchomiony na porcie ${PORT}`);
