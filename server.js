a37664f34b1b2b21a5e33eb3f53bb5c4a5a99a07
MaciejUlasiuk <filip123xd@o2.pl>
2025-06-08 19:55:54 +0200

    serverupdt

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
+const currencyRoutes = require('./routes/currencyRoutes'); 
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('testkantorkantor');
+  res.send('Witaj w Kantor API!');
 });
+
+app.use('/api', currencyRoutes); 
 
 app.listen(PORT, () => {
   console.log(`Serwer uruchomiony na porcie ${PORT}`);
-  console.log(`Otwórz w przeglądarce: http://localhost:${PORT}`);
 });
