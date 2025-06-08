f85047bc77d15b10bff91763e1dd4fb8b9901bd3
MaciejUlasiuk <filip123xd@o2.pl>
2025-06-08 22:56:23 +0200

    test middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
+router.get('/me', protect, (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            id: req.user.id,
+            email: req.user.email,
+            
+        });
+    } else {
+        res.status(401).json({ message: 'Nieautoryzowany dostęp.' });
+    }
+});
 
 module.exports = router;
